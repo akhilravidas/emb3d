b8aab1826b5a5de19b6d19f07623cc8f0114524f
Akhil Ravidas <ar@mod0.ai>
2023-10-05 21:30:02 -0700

    Remove dead code

--- emb3d/reader.py
+++ emb3d/reader.py
@@ -2,18 +2,7 @@
 Readers
 """
 import json
-from pathlib import Path
 from typing import Iterator, TextIO
-
-# def line(fname: Path):
-#     with fname.open() as f:
-#         for line in f:
-#             yield line.strip()
-
-
-# def jsonl(fname: Path):
-#     for nxt_line in line(fname):
-#         yield json.loads(nxt_line)
 
 
 def line(f_io: TextIO) -> Iterator[str]:
